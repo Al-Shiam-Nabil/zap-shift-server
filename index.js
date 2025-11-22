5174c307b8c86264ece2902c2f9ccb926415c588
Al Shiam Nabil <nabil15-4777@diu.edu.bd>
2025-11-22 11:10:20 +0600

    get parcel by id

--- index.js
+++ index.js
@@ -63,6 +63,15 @@ async function run() {
       res.json(result);
     });
 
+    // get percel by id
+
+    app.get("/parcels/:id", async (req, res) => {
+      const { id } = req.params;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelCollection.findOne(query);
+      res.json(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
